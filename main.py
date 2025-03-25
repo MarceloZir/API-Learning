d04836fed9f98993a27a3ca2d5af88ac23c68627
Marcelo Gabriel Zir <marcelogabrielzir@gmail.com>
2025-03-24 22:44:02 -0300

    Nova Branch
    Foi criada uma nova branch para adicionar novos livros no dicionário.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'título': 'O Senhor do Aneis - A Sociedade do Anel',
+        'autor': 'J.R.R Tokien'
+    }, 
+]
