f108b5a2fe937ffeb92beb920502da5ba1772d8e
Marcelo Gabriel Zir <marcelogabrielzir@gmail.com>
2025-03-24 23:14:31 -0300

    Editar Livros
    Adicionei um código que permite editar um livro na API, por meio do 'id', ao ser acessada pelo caminho /livros/<id>, utilizando o método PUT. Ao ser enviada uma requisição para esse caminho, com o id do livro e os dados a serem alterados no corpo da requisição, o livro correspondente será atualizado com as novas informações e retornado em formato JSON. Por exemplo, ao acessar /livros/1 e enviar um corpo com os novos dados do livro, o livro com id=1 será alterado e retornado atualizado.

--- main.py
+++ main.py
@@ -29,5 +29,13 @@ def obter_livro_por_id(id):
     for livro in livros:
         if livro.get('id') == id:
             return jsonify(livro)
+        
+@app.route('/livros/<int:id>',methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+            return jsonify(livros[indice])
 
 app.run(port=5000,host='localhost',debug=True)
