8bc7a2e3b46ec4a2c54645effcaa336d0ac2dbcc
Marcelo Gabriel Zir <marcelogabrielzir@gmail.com>
2025-03-24 22:57:50 -0300

    Obter Livros
    Adicionei um código que cria uma API com Flask que, ao ser acessada pelo caminho /livros, retorna uma lista de livros em formato JSON.

--- main.py
+++ main.py
@@ -8,4 +8,20 @@ livros = [
         'título': 'O Senhor do Aneis - A Sociedade do Anel',
         'autor': 'J.R.R Tokien'
     }, 
+    {
+        'id': 2,
+        'título': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling'
+    }, 
+    {
+        'id': 3,
+        'título': 'James Clear',
+        'autor': 'Hábitos Atômicos'
+    }, 
 ]
+
+@app.route('/livros')
+def obter_livros ():
+    return jsonify(livros)
+
+app.run(port=5000,host='localhost',debug=True)
