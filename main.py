bd0340ff3c446f50ab416f72f3aa879af7214305
Marcelo Gabriel Zir <marcelogabrielzir@gmail.com>
2025-04-05 13:31:46 -0300

    - Atualização do arquivo Main.py, agora utilizando fastapi em vez do flask. - Adição do Dockerfile e requirements.txt.

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-from flask import Flask, jsonify, request
+from fastapi import FastAPI, HTTPException
+from typing import List
 
-app = Flask(__name__)
+app = FastAPI()
 
 livros = [
     {
         'id': 1,
         'título': 'O Senhor do Aneis - A Sociedade do Anel',
         'autor': 'J.R.R Tokien'
-    }, 
+    },
     {
         'id': 2,
         'título': 'Harry Potter e a Pedra Filosofal',
         'autor': 'J.K Howling'
-    }, 
+    },
     {
         'id': 3,
         'título': 'James Clear',
         'autor': 'Hábitos Atômicos'
-    }, 
+    },
 ]
 
-@app.route('/livros',methods=['GET'])
-def obter_livros ():
-    return jsonify(livros)
+@app.get("/livros")
+def obter_livros():
+    return livros
 
-@app.route('/livros/<int:id>',methods=['GET'])
-def obter_livro_por_id(id):
+@app.get("/livros/{id}")
+def obter_livro_por_id(id: int):
     for livro in livros:
         if livro.get('id') == id:
-            return jsonify(livro)
-        
-@app.route('/livros/<int:id>',methods=['PUT'])
-def editar_livro_por_id(id):
-    livro_alterado = request.get_json()
-    for indice,livro in enumerate(livros):
+            return livro
+    raise HTTPException(status_code=404, detail="Livro não encontrado")
+
+@app.put("/livros/{id}")
+def editar_livro_por_id(id: int, livro_alterado: dict):
+    for indice, livro in enumerate(livros):
         if livro.get('id') == id:
             livros[indice].update(livro_alterado)
-            return jsonify(livros[indice])
-
-app.run(port=5000,host='localhost',debug=True)
+            return livros[indice]
+    raise HTTPException(status_code=404, detail="Livro não encontrado")
